5afb57e48c4aae0e9a40f3869827570805998bdd
SashaPlavi <sasha.bingulac@gmail.com>
2020-07-02 09:19:19 +0200

    crud

---
+++ app/routes/note_routes.js
@@ -0,0 +1,69 @@
+const { ObjectID } = require("mongodb");
+
+let objectId = require("mongodb").objectId;
+
+module.exports = function (app, db) {
+  const collection = db.db("notesdb").collection("notes");
+
+  app.get("/notes/:id", (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectID(id) };
+
+    collection.findOne(details, (err, item) => {
+      if (err) {
+        res.send("err");
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  app.put("/notes/:id", (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectID(id) };
+    const note = {
+      text: req.body.body,
+      title: req.body.title,
+    };
+
+    collection.update(details, note, (err, item) => {
+      if (err) {
+        res.send("err");
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  app.delete("/notes/:id", (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectID(id) };
+
+    collection.deleteOne(details, (err) => {
+      if (err) {
+        console.log(err);
+        res.send("err");
+      } else {
+        res.send("note " + id + " deleted");
+      }
+    });
+  });
+
+  app.post("/notes", (req, res) => {
+    // const collection = db.db("notesdb").collection("notes");
+    const note = {
+      text: req.body.body,
+      title: req.body.title,
+    };
+    collection.insertOne(note, (err, result) => {
+      if (err) {
+        console.log(err);
+
+        res.send("err");
+      } else {
+        res.send(result.ops[0]);
+        //console.log(err);
+      }
+    });
+  });
+};
